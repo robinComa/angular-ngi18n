9208cb9da1dc730ffd9f67b86d92c8b38170068f
robinComa <robin.coma.public@gmail.com>
2013-08-22 23:24:43 +0200

    Web site is ready

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('angularNgi18nApp', ['ngI18n'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
